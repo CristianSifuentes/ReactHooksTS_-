fd95b63ab5c775e59c2dc7e9528cd2fe2eccc73c
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-11-12 23:40:11 -0600

    working with useState, useEffect, useRef and customHooks

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,19 @@ import { createRoot } from 'react-dom/client'
 
 
 import './index.css'
-import { HooksApp } from './HooksApp'
+import { PokemonPage } from './03-examples/PokemonPage'
+// import { PokemonPage } from './03-examples/PokemonPage'
+// import { HooksApp } from './HooksApp'
+// import { TrafficLight } from './01-useState/TrafficLight'
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    {/* <TrafficLight /> */}
+    {/* <TrafficLightWithEffect /> */}
+    {/* <TrafficLightWithHook /> */}
+    <PokemonPage />
   </StrictMode>,
 )
